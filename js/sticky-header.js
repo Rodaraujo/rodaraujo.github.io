d5419f169790631b011769417f931d4d7cc3f826
Rodrigo Araujo <rodaraujo88@gmail.com>
2019-03-14 12:08:09 -0600

    2019 site
    new design blue+grey

--- js/sticky-header.js
+++ js/sticky-header.js
@@ -1,45 +1,31 @@
+ 
 $(document).ready(function(){
-  var navHeight = $("#rod-nav-bg").outerHeight();
-  var distance = navHeight - 72;
-  var isFaded = false;
+  var st = 0;
+  var lastScrollTop = 0;
+  var canFadeIn = true; 
+  var isMenuHidden = false;
+  var titleHeight = 40;
+  $("#rod-nav").fadeOut(0);
+  $("#rod-nav").addClass("rod-nav");
+
+
   $(window).scroll(function() {
-    if ($(document).scrollTop() > distance) {
-      var newMargin = "-" + distance.toString() + "px";
-      $("#rod-nav-bg").addClass("rod-sticky");
-      $("#rod-nav-bg").css("top", newMargin);
-      $("#rod-nav-spacer").height(navHeight);
+    st = $(document).scrollTop();
+
+
+    if (st > titleHeight){
+      if (canFadeIn){
+        $("#rod-nav").fadeIn(150);
+        canFadeIn = false;
+      }
+
     } else {
-      $("#rod-nav-bg").removeClass("rod-sticky")
-      $("#rod-nav-bg").css("top", "0");
-      $("#rod-nav-spacer").height(0);
-
+      if (!canFadeIn){
+        $("#rod-nav").fadeOut(150);
+        canFadeIn = true;
+      }
     }
-    if ($(document).scrollTop() > distance/3 && !isFaded) {
-$("#rod-title").animate({bottom: 20, opacity: 0.0}, 250);
-      //$("#rod-title").fadeTo(3500, 0.01);
-      isFaded = true;
-    }
-    if ($(document).scrollTop() < distance/3 && isFaded) {
-      $("#rod-title").fadeTo(250, 1);
-      $("#rod-title").css("bottom", 0);
-      isFaded = false;
-    }
+    lastScrollTop = st;
   });
-
-  $(window).resize(function(){
-    var navHeight = $("#rod-nav-bg").outerHeight();
-    var distance = navHeight - 72;
-  });
-
-  $(".rod-animate,:focus").hover (function(){
-    $( this ).animate({bottom: 3 }, 150);
-	$( this ).removeClass("shadow-out");
-    $( this ).addClass("shadow-in");
-  },
-  function(){
-    $( this ).animate({bottom: 0 }, 150);
-    $( this ).removeClass("shadow-in");
-	$( this ).addClass("shadow-out");
-  });
-
 });
+
